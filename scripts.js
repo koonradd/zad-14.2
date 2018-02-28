e8cbfee5ad7079b5376aa4ac688fef67a9355093
Konrad Kościelski <konrad.koscielski@interia.pl>
2018-02-28 21:05:54 +0100

    init

--- scripts.js
+++ scripts.js
@@ -8,53 +8,39 @@ var movies = [
     id: 1, 
     title: 'Harry Potter',
     desc: 'film o czarodzieju',
-<<<<<<< HEAD
     img: 'https://marketplace.canva.com/MACGFoEVNCs/1/0/thumbnail_large/canva-light-blue-cute-car-cartoon-car-wash-poster-MACGFoEVNCs.jpg'
-=======
-    img: './1.jpg'
->>>>>>> ccec667e78a0bd6feb23e660d6c9fb2047d63c7e
+
   },
   {
     id: 2,
     title: 'Król Lew',
     desc: 'Film o królu sawanny',
-<<<<<<< HEAD
     img: 'https://marketplace.canva.com/MACGFoEVNCs/1/0/thumbnail_large/canva-light-blue-cute-car-cartoon-car-wash-poster-MACGFoEVNCs.jpg'
-=======
-    img: './1.jpg'
->>>>>>> ccec667e78a0bd6feb23e660d6c9fb2047d63c7e
+
+
   },
   {
     id: 3,
     title: 'Film 3',
     desc: 'Film o js',
-<<<<<<< HEAD
+
     img: 'https://marketplace.canva.com/MACGFoEVNCs/1/0/thumbnail_large/canva-light-blue-cute-car-cartoon-car-wash-poster-MACGFoEVNCs.jpg'
-=======
-    img: './1.jpg'
->>>>>>> ccec667e78a0bd6feb23e660d6c9fb2047d63c7e
+
   },
   {
     id: 4,
     title: 'Film 4',
     desc: 'Film o js',
-<<<<<<< HEAD
+
     img: 'https://marketplace.canva.com/MACGFoEVNCs/1/0/thumbnail_large/canva-light-blue-cute-car-cartoon-car-wash-poster-MACGFoEVNCs.jpg'
-=======
-    img: './1.jpg'
->>>>>>> ccec667e78a0bd6feb23e660d6c9fb2047d63c7e
   },
   {
     id: 5,
     title: 'Film 5',
     desc: 'Film o js',
-<<<<<<< HEAD
     img: 'https://marketplace.canva.com/MACGFoEVNCs/1/0/thumbnail_large/canva-light-blue-cute-car-cartoon-car-wash-poster-MACGFoEVNCs.jpg'
-=======
-    img: './1.jpg'
->>>>>>> ccec667e78a0bd6feb23e660d6c9fb2047d63c7e
-   
-  }
+
+   }
   
     
 ];
@@ -65,13 +51,11 @@ var movies = [
 var moviesElements = movies.map(function(movie) {
   return React.createElement('li', {key: movie.id},
       React.createElement('h2', {}, movie.title),
-<<<<<<< HEAD
+
       React.createElement('p', {}, movie.desc),
      React.createElement('img', {src: movie.img}),
-=======
-      React.createElement('p', {}, movie.desc)
-       React.createElement('img', {}, movie.img)
->>>>>>> ccec667e78a0bd6feb23e660d6c9fb2047d63c7e
+
+
     );
 });
 
